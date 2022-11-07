a78d7ad634819e7e7f446b6ccffb65cbb5d45239
Tatiana Levchenko <medvedovskaya.tatiana@gmail.com>
2022-11-07 23:43:21 +0300

    hw9

---
+++ src/test/java/ru/netology/RadioTest.java
@@ -0,0 +1,10 @@
+package ru.netology;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RadioTest {
+
+    
+}
